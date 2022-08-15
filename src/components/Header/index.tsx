4099437ec41f01f804d9179d11291e28512ab9ed
Nicholas Costa <nicholascostadev@gmail.com>
2022-08-14 23:59:38 -0300

    Adjust padding on header when on mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,37 +10,36 @@ export function Header() {
     <Flex
       boxShadow={colorMode === 'dark' ? 'initial' : 'sm'}
       as="header"
-      w="100%"
-      justify="space-between"
-      align="center"
       py={5}
       px={globalPaddingForContainer}
     >
-      <Heading
-        // color={colorMode === 'dark' ? 'initial' : 'white'}
-        fontSize={['lg', 'xl', '2xl']}
-      >
-        Your sensitivity readable
-      </Heading>
-      {colorMode === 'dark' ? (
-        <Button
-          gap="2"
-          onClick={toggleColorMode}
-          disabled={!stateOfToggleColorscheme}
+      <Flex px={['5', '0']} w="100%" justify="space-between" align="center">
+        <Heading
+          // color={colorMode === 'dark' ? 'initial' : 'white'}
+          fontSize={['lg', 'xl', '2xl']}
         >
-          <Sun />
-          {!stateOfToggleColorscheme && <Text>In progress...</Text>}
-        </Button>
-      ) : (
-        <Button
-          gap="2"
-          onClick={toggleColorMode}
-          disabled={!stateOfToggleColorscheme}
-        >
-          <Moon />
-          {!stateOfToggleColorscheme && <Text>In progress...</Text>}
-        </Button>
-      )}
+          Your sensitivity readable
+        </Heading>
+        {colorMode === 'dark' ? (
+          <Button
+            gap="2"
+            onClick={toggleColorMode}
+            disabled={!stateOfToggleColorscheme}
+          >
+            <Sun />
+            {!stateOfToggleColorscheme && <Text>In progress...</Text>}
+          </Button>
+        ) : (
+          <Button
+            gap="2"
+            onClick={toggleColorMode}
+            disabled={!stateOfToggleColorscheme}
+          >
+            <Moon />
+            {!stateOfToggleColorscheme && <Text>In progress...</Text>}
+          </Button>
+        )}
+      </Flex>
     </Flex>
   )
 }
